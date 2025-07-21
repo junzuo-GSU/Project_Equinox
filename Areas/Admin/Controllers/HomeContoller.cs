376a8bbfbb269f502f9442eb4a0c4d7f8afcd3a9
junzuo-GSU <jzuo@govst.edu>
2025-07-21 16:04:11 -0500

    Update HomeContoller.cs

--- Areas/Admin/Controllers/HomeContoller.cs
+++ Areas/Admin/Controllers/HomeContoller.cs
@@ -5,6 +5,7 @@ namespace Equinox.Areas.Admin.Controllers
     [Area("Admin")]
     public class HomeController : Controller
     {
+        //This requiremet is to use attribute routing for actions defined in Admin area
         public IActionResult Index() => View();
     }
 }
